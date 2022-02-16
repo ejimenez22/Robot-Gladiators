52721c25c9f716f03648d05fc0f89c1b2bd7d6c3
Enrique Jimenez <ejimenez2292@gmail.com>
2022-02-15 22:03:15 -0600

    added multiple rounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,7 +17,8 @@ for(var i = 0; i < enemyNames.length; i++)
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function(enemyName) {
+var fight = function(enemyNames) {
+    window.alert ("Welcome to Robot Gladiators!");
     while (playerHealth > 0 && enemyHealth > 0) {
       // ask player if they'd like to fight or run
       var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -40,12 +41,12 @@ var fight = function(enemyName) {
       // remove enemy's health by subtracting the amount set in the playerAttack variable
       enemyHealth = enemyHealth - playerAttack;
       console.log(
-        playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
+        playerName + ' attacked ' + enemyNames + '. ' + enemyNames + ' now has ' + enemyHealth + ' health remaining.'
       );
   
       // check enemy's health
       if (enemyHealth <= 0) {
-        window.alert(enemyName + ' has died!');
+        window.alert(enemyNames + ' has died!');
   
         // award player money for winning
         playerMoney = playerMoney + 20;
@@ -53,13 +54,13 @@ var fight = function(enemyName) {
         // leave while() loop since enemy is dead
         break;
       } else {
-        window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
+        window.alert(enemyNames + ' still has ' + enemyHealth + ' health left.');
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
       playerHealth = playerHealth - enemyAttack;
       console.log(
-        enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
+        enemyNames + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
   
       // check player's health
@@ -69,9 +70,18 @@ var fight = function(enemyName) {
         break;
       } else {
         window.alert(playerName + ' still has ' + playerHealth + ' health left.');
-      }
+      }if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round" + (i + 1) );
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
     }
-  };
+     else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+     }
+    }
+};
 
 for(var i = 0; i < enemyNames.length; i++) {
     var pickedEnemyName = enemyNames[i];
